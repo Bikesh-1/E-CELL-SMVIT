90ff6c02a827d8e7b34d1bb815e2342818a72b84
Bikesh <iambikeshkumar07@gmail.com>
2024-11-09 00:33:27 +0530

    adding verical scorlling

--- script.js
+++ script.js
@@ -153,3 +153,43 @@ function validateForm() {
     alert("Message sent successfully!");
     return true;
 }
+
+
+// Initialize ScrollTrigger with Locomotive Scroll
+gsap.registerPlugin(ScrollTrigger);
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true
+});
+
+locoScroll.on("scroll", ScrollTrigger.update);
+
+ScrollTrigger.scrollerProxy("#main", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  },
+  getBoundingClientRect() {
+    return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
+  },
+  pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+});
+
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+ScrollTrigger.refresh();
+
+
+
+gsap.to("#page3 h1", {
+    x: "-80%",
+    scrollTrigger: {
+        trigger: "#page3",
+        scroller: "#main",
+        markers: true,
+        start: "top top",
+        end: "top -100%",
+        scrub: 1,
+        pin: true,
+    }
+});
